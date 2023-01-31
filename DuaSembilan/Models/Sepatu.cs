c010591e6ded1c4cb6bdb778d958ee05508e12a8
aysmd <aysmd@JON-LEGION>
2023-01-31 17:39:51 +0700

    Add project files.

---
+++ DuaSembilan/Models/Sepatu.cs
@@ -0,0 +1,12 @@
+﻿namespace DuaSembilan.Models
+{
+    public class Sepatu
+    {
+        public JenisSepatu JenisSepatu { get; set; }
+
+        public Sepatu(JenisSepatu jenisSepatu)
+        {
+            JenisSepatu = jenisSepatu;
+        }
+    }
+}
